c7e9ad4e0e73dd30ef2395eacdee445898cf1ae1
Gurdeep Singh <137512027+Romio1310@users.noreply.github.com>
2024-08-17 21:49:52 +0530

    Update MyToken.sol

--- xyz.sol
+++ xyz.sol
@@ -10,6 +10,10 @@ contract MyToken is ERC20, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+    function transferToken(address _receiver, uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "Insufficient balance");
+        _transfer(msg.sender, _receiver, _value);
+    }
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
